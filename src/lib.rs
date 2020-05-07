b038b29df81f111a7157124538afe8349592616b
Taylor Fraley <tfraley@clearent.com>
2020-05-07 12:15:53 -0400

    add method to create deps; move logic

--- src/lib.rs
+++ src/lib.rs
@@ -3,90 +3,99 @@ use serde_xml_rs::{from_str};
 use std::fs::{File, read_dir};
 use std::io::prelude::*;
 use std::path::Path;
-use std::ffi::{OsStr};
 
 #[derive(Debug, Serialize, Deserialize, PartialEq)]
 pub struct PackageReference {
-    #[serde(alias = "Include", default)]
-    pub include: String,
+  #[serde(alias = "Include", default)]
+  pub include: String,
 
-    #[serde(alias = "Version", default)]
-    pub version: String,
+  #[serde(alias = "Version", default)]
+  pub version: String,
 }
 
 #[derive(Debug, Serialize, Deserialize, PartialEq)]
 struct ItemGroup {
-    #[serde(alias = "PackageReference", default)]
-    pub dependencies: Vec<PackageReference>,
+  #[serde(alias = "PackageReference", default)]
+  pub dependencies: Vec<PackageReference>,
 }
 
 #[derive(Debug, Serialize, Deserialize, PartialEq)]
 struct Project {
-    #[serde(rename = "ItemGroup", default)]
-    pub item_groups: Vec<ItemGroup>,
+  #[serde(rename = "ItemGroup", default)]
+  pub item_groups: Vec<ItemGroup>,
 }
 
 #[derive(Debug, Serialize, PartialEq)]
 pub struct Deps {
-    pub name: String,
-    pub dependencies: Vec<PackageReference>,
+  pub name: String,
+  pub dependencies: Vec<PackageReference>,
 }
 
 impl Deps {
-    fn new(in_name: &OsStr) -> Deps {
-        Deps {
-            name : in_name.to_os_string().into_string().unwrap(),
-            dependencies : vec!(),
-        }
+  fn new(in_name: &str) -> Deps {
+      Deps {
+          name : in_name.to_string(), //in_name.to_os_string().into_string().unwrap(),
+          dependencies : vec!(),
+      }
+  }
+
+  fn from_proj_str(name: &str, proj: &mut Project) -> Result<Deps, serde_xml_rs::Error> {
+    let mut deps = Deps::new(name); 
+
+    for item_group in &mut proj.item_groups {
+      if item_group.dependencies.len() > 0 {
+        deps.dependencies.append(&mut item_group.dependencies);
+      }
     }
+
+    Ok(deps)
+  }
 }
 
 #[derive(Debug, Serialize)]
 pub struct ProjectCollection<'a> {
-    pub content: &'a Vec<Deps>,
-    pub project_count: &'a usize,
+  pub content: &'a Vec<Deps>,
+  pub project_count: &'a usize,
 }
 
 pub fn rec_read_dir(input_path: &Path) -> Result<Vec<Deps>, std::io::Error> {
-    let mut deps_vec: Vec<Deps> = vec!();
-    let entries = read_dir(input_path)?;
-    for dir in entries {
-      match dir {
-        Err(why) => return Err(why),
-        Ok(path) => {
-          if path.path().is_dir() {
-              deps_vec.append(&mut rec_read_dir(&path.path())?);
-          }
-          if let Some(extension) = path.path().extension(){
-              if extension == "csproj" {
-                  deps_vec.push(read_csproj(&path.path()));
+  let mut deps_vec: Vec<Deps> = vec!();
+  let entries = read_dir(input_path)?;
+  for dir in entries {
+    match dir {
+      Err(why) => return Err(why),
+      Ok(path) => {
+        if path.path().is_dir() {
+            deps_vec.append(&mut rec_read_dir(&path.path())?);
+        }
+        if let Some(extension) = path.path().extension(){
+          if extension == "csproj" {
+            match from_str(&read_csproj(&path.path())?) {
+              Err(why) => println!("{:?}", why),
+              Ok(mut proj) => {
+                let deps = 
+                  Deps::from_proj_str(
+                    &path.path().file_stem().unwrap().to_os_string().into_string().unwrap(), 
+                    &mut proj).unwrap();
+                deps_vec.push(deps);
               }
+            }
           }
         }
       }
     }
-    Ok(deps_vec)
+  }
+  Ok(deps_vec)
 }
 
-fn read_csproj(path: &Path) -> Deps {
-    let mut file = File::open(path).unwrap();
-    let mut csproj = String::new();
-    file.read_to_string(&mut csproj).unwrap();
-    if csproj.starts_with("\u{feff}") {
-      csproj = csproj.split_off(3);
+fn read_csproj(path: &Path) -> Result<String, std::io::Error> {
+    let mut file = File::open(path)?;
+    let mut proj = String::new();
+    file.read_to_string(&mut proj)?;
+
+    if proj.starts_with("\u{feff}") {
+      proj = proj.split_off(3);
     }
-
-    let proj: Project = from_str(&csproj).unwrap();
-
-    let mut deps = Deps::new(path.file_stem().unwrap().clone()); 
-
-    for item_group in proj.item_groups {
-      if item_group.dependencies.len() > 0 {
-        for dep in item_group.dependencies {
-          deps.dependencies.push(dep);
-        }
-      }
-    }
-
-    deps
+    Ok(proj)
 }
+
--- src/main.rs
+++ src/main.rs
@@ -6,38 +6,38 @@ use csdeps::{Deps, ProjectCollection, rec_read_dir};
 
 #[derive(StructOpt, Debug)]
 struct Opt {
-    /// Directory to search for project files
-    #[structopt(name = "DIR", parse(from_os_str))]
-    dir: PathBuf,
+  /// Directory to search for project files
+  #[structopt(name = "DIR", parse(from_os_str))]
+  dir: PathBuf,
 
-    /// Output using json format
-    #[structopt(short, long)]
-    json: bool,
+  /// Output using json format
+  #[structopt(short, long)]
+  json: bool,
 }
 
 fn main() -> std::io::Result<()> {
-    let opt = Opt::from_args();
-    let deps = rec_read_dir(opt.dir.as_path())?;
-    handle_deps_with_opt(deps, opt);
-    Ok(())
+  let opt = Opt::from_args();
+  let deps = rec_read_dir(opt.dir.as_path())?;
+  handle_deps_with_opt(deps, opt);
+  Ok(())
 }
 
 fn handle_deps_with_opt(deps: Vec<Deps>, opt: Opt) {
-    if opt.json  {
-        let coll: ProjectCollection = ProjectCollection {
-          content: &deps,
-          project_count: &deps.len(),
-        };
-        
-        let j = serde_json::to_string_pretty(&coll).unwrap();
-        println!("{}", j);
+  if opt.json  {
+    let coll: ProjectCollection = ProjectCollection {
+      content: &deps,
+      project_count: &deps.len(),
+    };
+    
+    let j = serde_json::to_string_pretty(&coll).unwrap();
+    println!("{}", j);
+  }
+  else {
+    for dep in deps.iter() {
+      println!("\nProject name: {}", dep.name);
+      for reference in dep.dependencies.iter() {
+        println!("{} version: {}", reference.include, reference.version);
+      }
     }
-    else {
-        for dep in deps.iter() {
-          println!("\nProject name: {}", dep.name);
-          for reference in dep.dependencies.iter() {
-            println!("{} version: {}", reference.include, reference.version);
-          }
-        }
-    }
+  }
 }
